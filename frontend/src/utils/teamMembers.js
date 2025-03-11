2cc00afb7ec92b9297f59e980f94dd4f6a733257
devton <anthoguerra2018@gmail.com>
2025-03-11 11:01:22 -0500

    fix: Update image path for Anthony in teamMembers

--- frontend/src/utils/teamMembers.js
+++ frontend/src/utils/teamMembers.js
@@ -1,30 +1,30 @@
 export const teamMembers = [
-    {
-        id: 1,
-        name: "Anthony Guerra",
-        role: "Estudiante de Ing. de Sistemas",
-        image: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80",
-        description: "",
-    },
-    {
-        id: 2,
-        name: "Harold Ormeño",
-        role: "Estudiante de Ing. de Sistemas",
-        image: "/images/Harold.webp",
-        description: "",
-    },
-    {
-        id: 3,
-        name: "Jhon Oré",
-        role: "Estudiante de Ing. de Sistemas",
-        image: "/images/Jhon.webp",
-        description: "",
-    },
-    {
-        id: 4,
-        name: "Daniel Olivares",
-        role: "Estudiante de Ing. de Sistemas",
-        image: "/images/Daniel.webp",
-        description: "",
-    },
+  {
+    id: 1,
+    name: "Anthony Guerra",
+    role: "Estudiante de Ing. de Sistemas",
+    image: "/images/Anthony.jpg",
+    description: "",
+  },
+  {
+    id: 2,
+    name: "Harold Ormeño",
+    role: "Estudiante de Ing. de Sistemas",
+    image: "/images/Harold.webp",
+    description: "",
+  },
+  {
+    id: 3,
+    name: "Jhon Oré",
+    role: "Estudiante de Ing. de Sistemas",
+    image: "/images/Jhon.webp",
+    description: "",
+  },
+  {
+    id: 4,
+    name: "Daniel Olivares",
+    role: "Estudiante de Ing. de Sistemas",
+    image: "/images/Daniel.webp",
+    description: "",
+  },
 ];
